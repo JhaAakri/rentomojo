e29c8809d2c9310bf567641d3827be4f35ebf8c6
Ajha30 <2gi19is001@students.git.edu>
2024-01-05 12:37:10 +0530

    Change Updated

---
+++ src/Pages/AllRoutes.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Productspage from "./productspage";
+
+const AllRoutes = () => {
+  return (
+    <div>
+     
+       
+        {/* <Footer /> */}
+
+    </div>
+  );
+};
+
+export default AllRoutes;
